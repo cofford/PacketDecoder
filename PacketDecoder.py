255e83b14996d998b51ec3d0f2abc46e09d469e1
Casey Offord <cofford@gmail.com>
2019-09-25 15:50:34 -0700

    gndspeed fixed.  Added print formatting.

--- PacketDecoder.py
+++ PacketDecoder.py
@@ -5,6 +5,8 @@ import struct
 data = bytes.fromhex('7e5b01ff0a0903001369ab8c05a5a77e') #time, date from GPS
 #data = bytes.fromhex('7e5b01ff0a09001369ab9c0552fd3c424e36f5c2000dfa06055e00c4a37e') #GPRMC time, date, position, mag var
 
+#unstuff bytes here.  7d5d = 7d, 7d5e = 7e
+
 protocol = data[1]
 source = data[2]
 destination = data[3]
@@ -23,13 +25,13 @@ checksum = data[-3:-1].hex()
 if packet_type == 9:
       subpacket_type = payload[0]
 
-      if subpacket_type == 0:
+      if subpacket_type == 0: #time, date, position, mag var, from current GPS source, like GPRMC
         time = int.from_bytes(payload[2:6], byteorder='little')
         latitude = struct.unpack("f", payload[6:10])[0]
         longitude = struct.unpack("f", payload[10:14])[0]
         track = struct.unpack("h", payload[14:16])[0]
         magvar = struct.unpack("h", payload[16:18])[0]
-        gndspeed = struct.unpack("h", payload[18:20])[0]
+        gndspeed = struct.unpack(">h", payload[18:20])[0]
      
         year = payload[1]   
         month = time & 15
@@ -43,19 +45,15 @@ if packet_type == 9:
         magvar = magvar/100
         gndspeed = gndspeed/10
 
-        print(subpacket_type)
-        print(year)
-        print(month)
-        print(day)
-        print(hour)
-        print(min)
-        print(sec)
-        print(status)
-        print(latitude)
-        print(longitude)
-        print(track)
-        print(magvar)   
-        print(gndspeed)
+        print('subpacket type: {}'.format(subpacket_type))
+        print('20{}/{}/{}, {}:{}:{}, Status: {}'.format(year, month, day, hour, min, sec, status))
+        print('latitude: {}, longitude: {}'.format(latitude, longitude))
+        print('track: {}\N{DEGREE SIGN}, gndspeed: {} kts, magvar: {}\N{DEGREE SIGN}'.format(track, gndspeed, magvar))
+        
+
+        #if subpacket_type == 1: navigation data to active waypoint, like GPRMB
+
+        #if subpacket_type == 2: waypoints in active flight plan from current GPS source
 
       if subpacket_type == 3:
         time = int.from_bytes(payload[3:7], byteorder='little')
@@ -68,13 +66,9 @@ if packet_type == 9:
         sec = (time >> 20) & 63
         status = (time >> 26) & 1
        
-        print(subpacket_type)
-        print(year)
-        print(month)
-        print(day)
-        print(hour)
-        print(min)
-        print(sec)
-        print(status)
+        print('subpacket type: {}'.format(subpacket_type))
+        print('20{}/{}/{}, {}:{}:{}, Status: {}'.format(year, month, day, hour, min, sec, status))
+
+        #if subpacket_type == 4: GPS altitide and geoidal difference, fix quality, number of satellites in fix, from current GPS source like GPGGA
         
         
