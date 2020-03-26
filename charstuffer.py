7e866cd17c22f7202b290b1149ab57fa206cc1df
cofford <cofford@gmail.com>
2020-03-26 14:48:20 -0700

    update charstuffer

--- charstuffer.py
+++ charstuffer.py
@@ -1,7 +1,3 @@
-#stuffed: 5B01FF0A0231383D3400687D5D
-#unstuffed: 5B01FF0A0231383D3400687D
-#nochecksum: 5B01FF0A0231383D3400
-
 #'7e5b01ff0a1a00010000000084c501f4aa010000000000c201d6af01000000001cce0188d5010000000006f301f4f5014e4e000000000000000000000000000000000000001f008002000000000000010200000000000000000014000000000000f7457e'
 
 import crcmod
@@ -15,14 +11,14 @@ stuffedbytes = '7d5d'
 
 def unstuff(payload):
     
-    x = payload.replace (escbyte, stuffedbytes)
+    x = payload[2:-2].replace (escbyte, stuffedbytes)
     y = x.replace (flagbyte, stuffedflag)
     return y #return value
 
 def stuff(payload):
     
     x = payload.replace (stuffedflag, flagbyte)
-    y = x.replace (stuffedbytes, escbyte)[2:-6]
+    y = x.replace (stuffedbytes, escbyte)
     return y #return value
 
 
@@ -30,16 +26,13 @@ payload = '7e5b01ff0a1a00010000000084c501f4aa010000000000c201d6af01000000001cce0
 #print(payload)
 unstuffed = unstuff(payload)
 print('unstuffed: ', unstuffed)
-stuffed = stuff(unstuffed)
+stuffed = stuff(unstuffed)[:-4]  
 print('stuffed: ', stuffed)
 crc_func = crcmod.predefined.mkCrcFun('x-25')
-hexpacket = binascii.unhexlify(stuffed)
-
-
-crc = crc_func(hexpacket)
-print(crc)
+crc = crc_func(binascii.unhexlify(stuffed))
+#print(crc)
 checksum = hex(struct.unpack("<H", struct.pack(">H", crc))[0])[2:]
 print(checksum)
-stuffed_payload = stuff(unstuffed + checksum)
+stuffed_payload = stuff(unstuffed[:-4] + checksum)  #removes existing checksum, adds new one
 print(payload)
 print(flagbyte + stuffed_payload + flagbyte)
